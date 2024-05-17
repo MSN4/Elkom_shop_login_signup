4057f02b486c707c425b25cc29283b875088dd56
MSN4 <86416068+MSN4@users.noreply.github.com>
2024-05-18 02:10:26 +0300

    a bug that needs to be fixed in the admin launching

--- app/src/main/java/com/javaapp/loginsignupxlm/AdminLogin.java
+++ app/src/main/java/com/javaapp/loginsignupxlm/AdminLogin.java
@@ -1,14 +1,34 @@
 package com.javaapp.loginsignupxlm;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AdminLogin extends AppCompatActivity {
+
+    private FirebaseAuth admin;
+    private FirebaseFirestore fStore;
+    private EditText adminEmail, adminPassword;
+
+    private Button loginBtn;
+    protected static Boolean isAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +36,75 @@ public class AdminLogin extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_login);
 
+        adminEmail =findViewById(R.id.edit_adminEmail);
+        adminPassword =findViewById(R.id.edit_adminPassword);
+        loginBtn=findViewById(R.id.adminloginButton);
+
+        admin =FirebaseAuth.getInstance();
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email= adminEmail.getText().toString();
+                String password= adminPassword.getText().toString();
+                //email is not empty and matches pattern
+                if(!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                    //password is not empty
+                    if(!password.isEmpty()){
+                        //if the user is admin
+
+                            admin.signInWithEmailAndPassword(email, password)
+                                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                                        @Override
+                                        public void onSuccess(AuthResult authResult) {
+                                            Toast.makeText(AdminLogin.this, "Login Successful! " + authResult.getUser().getUid(),
+                                                    Toast.LENGTH_LONG).show();
+                                            checkUserAccessLevel(authResult.getUser().getUid());
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(AdminLogin.this, "Login Failed!",
+                                                    Toast.LENGTH_LONG).show();
+                                        }
+
+                                    });
+                        }
+                    else
+                        adminPassword.setError("Password cannot be empty!");
+                }
+                else if (email.isEmpty())
+                    adminEmail.setError("Email cannot be empty!");
+                else
+                    adminEmail.setError("Please enter a valid email!");
+
+            }
+        });
+
+
     }
+
+    protected void checkUserAccessLevel(String uid) {
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        db.collection("users").document(uid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d("AdminLogin", "onSuccess: "+ documentSnapshot.getData());
+                if(documentSnapshot.getString("isAdmin")!=null){
+                   startActivity(new Intent(AdminLogin.this, AdminHome.class));
+                    isAdmin=true;
+                   finish();
+
+                }
+                else {
+                    Toast.makeText(AdminLogin.this, "Not an Admin!", Toast.LENGTH_LONG).show();
+                    isAdmin=false;
+                }
+            }
+        });
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/javaapp/loginsignupxlm/UserLogin.java
+++ app/src/main/java/com/javaapp/loginsignupxlm/UserLogin.java
@@ -80,7 +80,9 @@ public class UserLogin extends AppCompatActivity {
                                public void onSuccess(AuthResult authResult) {
                                    Toast.makeText(UserLogin.this,"Login Successful!",
                                            Toast.LENGTH_LONG).show();
-                                   startActivity(new Intent(UserLogin.this,MainActivity.class));
+
+
+                                   startActivity(new Intent(UserLogin.this,HomePage.class));
                                    finish();
                                }
                            }).addOnFailureListener(new OnFailureListener() {
@@ -153,13 +155,21 @@ public class UserLogin extends AppCompatActivity {
 
     //check if user is already logged in, if logged in open home page
     //if not, then open login
+    AdminLogin adminLogin = new AdminLogin();
+    boolean admin = adminLogin.isAdmin;
     @Override
     protected void onStart() {
         super.onStart();
-        if (auth.getCurrentUser() != null){
-            Toast.makeText(UserLogin.this, "Already Logged In!", Toast.LENGTH_SHORT).show();
-            startActivity(new Intent(UserLogin.this, HomePage.class));
-            finish(); //close login activity
+        if (auth.getCurrentUser() != null) {
+
+            if (admin) {
+                Toast.makeText(UserLogin.this, "Admin is already Logged In!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserLogin.this, AdminHome.class));
+            } else {
+                Toast.makeText(UserLogin.this, "Already Logged In!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(UserLogin.this, HomePage.class));
+            }
+            finish(); // close login activity
         }
     }
 
